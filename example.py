782ddd58fbfe06a5c88379255668077f7478ce88
Ivan Makarov <ivan.s.makarov@yandex.ru>
2018-01-29 22:51:14 +0300

    add solution

--- example.py
+++ example.py
@@ -1,34 +1,30 @@
 import requests
-
-API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
-URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-
-    :param to_lang:
-    :return:
-    """
-
-    params = {
-        'key': API_KEY,
-        'text': text,
-        'lang': 'ru-{}'.format(to_lang),
-    }
-
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-    return ''.join(json_['text'])
+import sys
 
 
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
+def translate_file(source_file, translation_file, from_lang, to_lang='ru'):
+    def translate_it(text, to_lang, from_lang):
+        API_KEY = 'trnsl.1.1.20180129T193855Z.7f826604e6fa6a11.e07ea3418d58e9fae8e826820ff47fb97454ad52'
+        URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
+        params = {
+            'key': API_KEY,
+            'lang': '{}-{}'.format(from_lang, to_lang),
+            'text': text
+        }
 
-requests.post('http://requestb.in/10vc0zh1', json=dict(a='goo', b='foo'))
+        response = requests.post(URL, params)
+
+        json_ = response.json()
+        return ''.join(json_['text'])
+
+    with open(source_file, encoding='utf-8') as file:
+        text = file.read()
+        translation = translate_it(text, to_lang, from_lang)
+
+        with open(translation_file, 'w', encoding='utf-8') as target_file:
+            target_file.write(translation)
+
+source_file, translation_file, from_lang, to_lang = sys.argv[1:]
+
+translate_file(source_file, translation_file, from_lang, to_lang)
